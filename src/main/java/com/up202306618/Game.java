ddf34c20253272b219282b117f6f14b4a717d60d
Claudio Meireles <up202306618@up.pt>
2024-10-11 11:00:17 +0100

    Configure Game Screen
    Configuration of Screen Font, Length and Height added.
    Screen initialization added.

--- src/main/java/com/up202306618/Game.java
+++ src/main/java/com/up202306618/Game.java
@@ -1,15 +1,33 @@
 package com.up202306618;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFontConfiguration;
 
 import java.io.IOException;
 
 public class Game {
-    private Screen screen;
+    private static final int LENGTH = 90;
+    private static final int HEIGHT = 30;
+    private final Screen screen;
+    private boolean run;
+    private int x = 10;
+    private int y = 10;
 
     Game() throws IOException {
-        this.screen = new DefaultTerminalFactory().createScreen();
+        this.screen = new DefaultTerminalFactory()
+                .setInitialTerminalSize(new TerminalSize(LENGTH, HEIGHT))
+                .setTerminalEmulatorFontConfiguration(
+                        SwingTerminalFontConfiguration.getDefaultOfSize(20)
+                )
+                .createScreen();
+        this.screen.setCursorPosition(null);
+        this.screen.startScreen();
+        this.screen.doResizeIfNecessary();
     }
 
     public void close() throws IOException {
