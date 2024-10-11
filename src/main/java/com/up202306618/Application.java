e3ec7d30d4614a5692198ff7a4a5704184b0936c
Claudio Meireles <up202306618@up.pt>
2024-10-11 14:00:57 +0100

    Remove extra imports from Application

--- src/main/java/com/up202306618/Application.java
+++ src/main/java/com/up202306618/Application.java
@@ -1,11 +1,5 @@
 package com.up202306618;
 
-import com.googlecode.lanterna.SGR;
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
 import java.io.IOException;
 
 public class Application {
